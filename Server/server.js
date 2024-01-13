9d4489d6d9d9f5dceb04ff58fce376e901d74bc7
HarshJ27 <harshkr2709@gmail.com>
2024-01-14 04:32:30 +0530

    first commit

---
+++ Server/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import fileUpload from "express-fileupload";
+
+const app = express();
+dotenv.config();
+
+app.use(fileUpload());
+app.use(express.json());
+app.use(cors());
+
+const PORT = 8001;
+
+// Database connect
+mongoose
+  .connect(process.env.MONGO_DB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
+
+import moviesController from './Controllers/MoviesController.js';
+
+app.use("/api/v1/moviecritic", moviesController);
+
+app.get("/", (req, res) => {
+  res.send("Hello SaasMonk!!!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
